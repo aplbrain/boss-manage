511245a76595fdf45c13ab5f7790e3647ae0dc8f
Luis Rodriguez <lrodri729@gmail.com>
2018-04-24 11:54:55 -0400

    set_vars update

--- config/set_vars.sh
+++ config/set_vars.sh
@@ -1,6 +1,6 @@
-export AWS_CREDENTIALS=~/saber/boss-manage/config/aws-credentials
+export AWS_CREDENTIALS=../config/aws-credentials
 export SSH_KEY=~/.ssh/boss-bossdb-20180330.pem
-#export SSH_KEY=~/.ssh/microns-bastion20151117.pem
+
 export BASTION_KEY=~/.ssh/boss-bastion-20180315.pem
 export BASTION_IP=52.7.186.170
 export BASTION_USER=ubuntu
